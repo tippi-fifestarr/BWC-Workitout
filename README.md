3cc307ec2b68550db5277624dc0ec0cde92653bb
tippi-fifestarr <62179036+tippi-fifestarr@users.noreply.github.com>
2020-11-08 02:00:57 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# BWC-Challenge
+ stable MVP
---
+++ cardcreator.js
@@ -0,0 +1,143 @@
+const cardsArr = [
+    {
+        "name" : "dadeucer",
+        "img" : "../CopyDD/images/dalogo.jpg",
+        "reps" : 2,
+        "sets" : 2
+    },
+    {
+        "name" : "fifestarr stretch",
+        "img" : "../CopyDD/images/invite-dadeuce.jpg",
+        "reps" : 1,
+        "sets" : 1
+    },
+    {
+        "name" : "lunches",
+        "img" : "../CopyDD/gifs/lunches.gif",
+        "reps" : 25,
+        "sets" : 2
+    },
+    {
+      "name" : "push-ups",
+      "img" : "../CopyDD/gifs/pushup.gif",
+      "reps" : 25,
+      "time" : null,
+      "sets" : 2
+    },
+    {
+      "name" : "Reverse Crunch",
+      "img" : "../CopyDD/gifs/reverse-crunch.gif",
+      "reps" : 50,
+      "time" : null,
+      "sets" : 2
+    },
+    {
+      "name" : "L Side-Oblique-Crunch",
+      "img" : "../CopyDD/gifs/Side-Oblique-Crunch.gif",
+      "reps" : 40,
+      "time" : null,
+      "sets" : 1
+    },
+    {
+      "name" : "Right side Crunck",
+      "img" : "../CopyDD/gifs/side-crunch.gif",
+      "reps" : 40,
+      "time" : null,
+      "sets" : 1
+    },
+    {
+      "name" : "regular Crunch",
+      "img" : "../CopyDD/images/mendelbrott-fractal.jpg",
+      "reps" :50,
+      "time" : null,
+      "sets" : 2
+    },
+    {
+      "name" : "close grip push ups",
+      "img" : "../CopyDD/gifs/Close-Grip-Push-Ups.gif",
+      "reps" : 25,
+      "time" : null,
+      "sets" : 2
+    },
+    {
+      "name" : "standing squats",
+      "img" : "../CopyDD/gifs/squats.gif",
+      "reps" : 50,
+      "time" : null,
+      "sets" : 2
+    }
+]
+
+var intensityX = prompt('intensity 0-1?');
+
+function allowDrop(ev) {
+    ev.preventDefault();
+  }
+  
+  function drag(ev) {
+    ev.dataTransfer.setData("text", ev.target.id);
+  }
+  
+  function drop(ev) {
+    ev.preventDefault();
+    var data = ev.dataTransfer.getData("text");
+    ev.target.appendChild(document.getElementById(data));
+  }
+
+// console.log(cardsArr)
+// we are mapping from cardsArr into hero (key value pairs)
+cardsArr.map(hero => {
+    // in swell foop, declare OBJECT DESTRUCTURING
+    const {name, img, reps, sets} = hero;
+    setsLeft = sets
+    console.log(sets)
+    // if (sets >= 1){
+        while (setsLeft >= 1){
+            console.log("the first part works")
+            makeDatCard(setsLeft, name, img, reps, sets);
+            console.log("maked card")
+            setsLeft--;
+        }
+    // }
+    // else{
+    //         makeDatCard(setsLeft, name, img, reps, sets)
+    // }
+})
+
+function makeDatCard(setsLeft, name, src, reps, sets){
+    console.log("makedat"+setsLeft);
+    // this is the card div, the card itself one could say.
+    const heroCard = document.createElement("div")
+    // in javascript, id is like "name"
+    heroCard.id = `${name}${setsLeft}`
+    heroCard.draggable="true"
+    // now we are filling the HTML in another feel swoop! ` c o  o   l` 
+    heroCard.innerHTML = `
+    <p> ${name} </p>
+    <img src='${src}' height=69 width=69>
+    <h3> ${reps*intensityX}=${reps}*${intensityX} reps <br>
+    set ${setsLeft} of ${sets}</h3>
+    `
+    // give it some class
+    heroCard.classList.add('card')
+    // id is the sequential number dan added to the heroes.js to help identify this. 
+    heroCard.classList.add(`set${setsLeft}`)
+    heroCard.onclick = function(){
+        heroCard.classList.add('hide')
+      }
+    // and append the card
+    console.log("something wrong with list1"+heroCard)
+    document.querySelector('#list1').appendChild(heroCard)
+}
+
+
+
+    
+    
+        
+        
+
+    // template i'm going for 
+    // <div  id='card1' class="card" draggable="true" ondragstart="dragStart(event)">
+    //         Work on article
+    // <img id="card0" src="img_logo.gif" draggable="true" ondragstart="drag(event)" width="336" height="69">
